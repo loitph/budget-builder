a7716508d1695a0b4f6865a0c58ba70313dd88f1
Loi Tran <tranloi.tphl@gmail.com>
2025-04-03 16:54:47 +0700

    Add new page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { PageUrl } from './shared/constant';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: PageUrl.dashboard,
+    pathMatch: 'full'
+  },
+  {
+    path: PageUrl.dashboard,
+    component: DashboardComponent
+  },
+];
---
+++ src/app/shared/constant.ts
@@ -0,0 +1,3 @@
+export const PageUrl = {
+  dashboard: 'dashboard'
+};
